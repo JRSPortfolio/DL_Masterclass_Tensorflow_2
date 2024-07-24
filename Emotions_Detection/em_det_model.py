9e3a1a44bd2dc6c46f7107007fd8de440e3ab5ec
JRSPortfolio <jrsalgueiro@live.com.pt>
2024-07-24 01:37:18 +0100

    added visualization testing

--- Emotions_Detection/em_det_model.py
+++ Emotions_Detection/em_det_model.py
@@ -2,17 +2,186 @@ import tensorflow as tf
 from data_preparation import SplitRatios, create_dataset, ds_shuffle_split
 from set_model import ModelConfigs, make_sequential_model
 from tensorflow.keras import Sequential #type:ignore
+from tensorflow.keras.optimizers import Adam  #type: ignore
+from tensorflow.keras.losses import CategoricalCrossentropy #type: ignore
+from tensorflow.keras.metrics import CategoricalAccuracy, Precision, Recall, TopKCategoricalAccuracy #type: ignore
+from tensorflow.keras.models import load_model #type: ignore
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    configs = ModelConfigs(batch_size = 1, image_size = (200, 200))
+def get_dataset(configs: ModelConfigs):
     ds = create_dataset(configs)
-
-    ratios = SplitRatios(0.7, 0.15, 0.15)
-
+    ratios = SplitRatios(0.8, 0.1, 0.1)
     train_ds, val_ds, test_ds = ds_shuffle_split(ds, ratios)
+    
+    return train_ds, val_ds, test_ds
+
+def make_model(train_ds, val_ds, configs: ModelConfigs):
+    metrics = [CategoricalAccuracy(name = 'accuracy'), TopKCategoricalAccuracy(k = 2, name = 'top_k_acc'), Precision(name = 'precision'),
+               Recall(name = 'recall')]
+    
+    model = make_sequential_model(configs)
+    
+    loss = CategoricalCrossentropy()
+    
+    model.compile(optimizer = Adam(learning_rate = configs['learning_rate']), loss = loss, metrics = metrics)
+    model.fit(train_ds, validation_data = val_ds, epochs = configs['epochs'], verbose = 1)
+    
+    return model
+    
+def show_model_metrics(model, test_ds, configs: ModelConfigs):
+    metrics =  pd.DataFrame(model.history.history)
+
+    fig01, axes01 = plt.subplots(1, 3, figsize = (48, 24))
+    plt.subplots_adjust(left = 0.04, right = 0.99, top = 0.99, bottom = 0.08)
+
+    sns.lineplot(metrics[['accuracy', 'val_accuracy', 'top_k_acc', 'val_top_k_acc']], ax = axes01[0])
+    axes01[0].set_xticks(range(configs['epochs']))
+    axes01[0].set_yticks(np.arange(0, 1.05, 0.05))
+    axes01[0].grid(True)
+    
+    sns.lineplot(metrics[['precision', 'recall', 'val_precision', 'val_recall']], ax = axes01[1])
+    axes01[1].set_xticks(range(configs['epochs']))
+    axes01[1].set_yticks(np.arange(0, 1.05, 0.05))
+    axes01[1].grid(True)
+    
+    sns.lineplot(metrics[['loss', 'val_loss']], ax = axes01[2])
+    axes01[2].set_xticks(range(configs['epochs']))
+    axes01[2].set_yticks(np.arange(0, 1.05, 0.05))
+    axes01[2].grid(True)
+    
+    print(f'Model eval: {model.evaluate(test_ds)}')
+    
+    plt.show()
+    
+# def test_random_image(model):
+#     folders = ['angry', 'happy', 'sad']
+#     r_folder = np.random.randint(0, 3)
+#     image_folder = f'Emotions_Detection/dataset/{folders[r_folder]}'
+#     r_image = np.random.randint(0, len(os.listdir(image_folder)))
+#     image = cv2.imread(f'{image_folder}/{r_image}.jpg')
+#     im = tf.constant(image, dtype = tf.float32)
+#     im = tf.expand_dims(im, axis = 0)
+#     print(folders[tf.argmax(model(im), axis = -1).numpy()[0]])
+    
+#     print(image_folder)
+#     print(r_image)
+#     plt.imshow(tf.constant(image, dtype = tf.float32))
+#     plt.show()
+    
+    
+    
+    
+
+def config_values():
+    # image_size = (200, 200)
+    # learning_rate = [0.01]
+    # epochs = [5]
+    # batch_size = 16
+    # conv2d_01_filters = [24]
+    # conv2d_01_kernel = [6]
+    # conv2d__01_strides = [1]
+    # conv2d_02_filters = [16]
+    # conv2d_02_kernel = [4]
+    # conv2d_02_strides = [1]
+    # conv2d_03_filters = [8]
+    # conv2d_03_kernel = [2]
+    # conv2d_03_strides = [1]
+    # dense_01 = [128]
+    # dense_02 = [32]
+    
+    # image_size = (350, 350)
+    # learning_rate = 0.005
+    # epochs = 300
+    # batch_size = 16
+    # conv2d_01_filters = 128
+    # conv2d_01_kernel = 8
+    # conv2d__01_strides = 2
+    # conv2d_02_filters = 64
+    # conv2d_02_kernel = 4
+    # conv2d_02_strides = 1
+    # conv2d_03_filters = 16
+    # conv2d_03_kernel = 2
+    # conv2d_03_strides = 1
+    # dense_01 = 128
+    # dense_02 = 64
+    # dense_03 = 32
+    
+    image_size = (300, 300)
+    learning_rate = 0.005
+    epochs = 200
+    batch_size = 16
+    conv2d_01_filters = 80
+    conv2d_01_kernel = 6
+    conv2d__01_strides = 2
+    conv2d_02_filters = 50
+    conv2d_02_kernel = 4
+    conv2d_02_strides = 1
+    conv2d_03_filters = 16
+    conv2d_03_kernel = 2
+    conv2d_03_strides = 1
+    dense_01 = 128
+    dense_02 = 32
+    dense_03 = 6
+    
+    configs = ModelConfigs (image_size = image_size, learning_rate = learning_rate, epochs = epochs,
+                            batch_size = batch_size, conv2d_01_filters = conv2d_01_filters, conv2d_01_kernel = conv2d_01_kernel,
+                            conv2d_01_strides = conv2d__01_strides, conv2d_02_filters = conv2d_02_filters, conv2d_02_kernel = conv2d_02_kernel,
+                            conv2d_02_strides = conv2d_02_strides, conv2d_03_filters = conv2d_03_filters, conv2d_03_kernel = conv2d_03_kernel,
+                            conv2d_03_strides = conv2d_03_strides, dense_01 = dense_01, dense_02 = dense_02, dense_03 = dense_03)
+    
+    return configs
+
+def save_model(model, name: str):
+    model.save(f'Emotions_Detection/keras_models/{name}.keras')
+
+def transform_test(test_ds):
+    test_labels = []
+    test_data = []
+    
+    for batch in test_ds.as_numpy_iterator():
+        data_batch, label_batch = batch
+        for data, label in zip(data_batch, label_batch):
+            test_labels.append(label)
+            test_data.append(data)
+
+    test_labels = np.array(test_labels)
+    test_data = np.array(test_data)
+    
+    return test_data, test_labels
+  
+def test_model(model, test_ds):
+    labels = ['angry', 'happy', 'sad']
+    test_data, test_labels = transform_test(test_ds)
+    
+    random_list = np.random.randint(0, len(test_data), 24).tolist()
+    fig, axes = plt.subplots(4, 6, figsize = (36, 18))
+    plt.subplots_adjust(left = 0.04, right = 0.96, top = 0.95, bottom = 0.1, wspace = 0.6, hspace = 0.6)
+
+    ax_x = int(0)
+    ax_y = int(0)
+    for i in random_list:
+        image = np.array(test_data[i]).reshape(1, 300, 300, 3)
+        pred = model(image, training = False)
+        axes[ax_x, ax_y].imshow(test_data[i] / 255)
+        axes[ax_x, ax_y].set_title(f"Real: {labels[np.argmax(np.round(test_labels[i]).astype(int))]} | Pred: {labels[np.argmax(np.round(np.array(pred)).astype(int))]}")
+        ax_y += 1
+        if ax_y == 6:
+            ax_y = 0
+            ax_x += 1
+                    
+    plt.show()
+    
+if __name__ == '__main__':
+    configs = config_values()
+    train_ds, val_ds, test_ds = get_dataset(configs)
+    model = make_model(train_ds, val_ds,configs)
+    show_model_metrics(model, test_ds, configs)
+    # save_model(model, 'test_01')
+
+    # model = load_model('Emotions_Detection/keras_models/test_01.keras')
+    test_model(model, test_ds)
+
--- Emotions_Detection/set_model.py
+++ Emotions_Detection/set_model.py
@@ -1,12 +1,13 @@
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, InputLayer #type: ignore
 from tensorflow.keras.models import Sequential #type: ignore
+from tensorflow.keras.layers import Resizing, Rescaling #type: ignore
 
 class ModelConfigs(dict):
     def __init__(self, name = None, method = None, metrics = None, image_size = None, learning_rate = None,
                  epochs = None, batch_size = None, conv2d_01_filters = None, conv2d_01_kernel = None,
-                 conv2d__01_strides = None, conv2d_02_filters = None, conv2d_02_kernel = None,
+                 conv2d_01_strides = None, conv2d_02_filters = None, conv2d_02_kernel = None,
                  conv2d_02_strides = None, conv2d_03_filters = None, conv2d_03_kernel = None,
-                 conv2d_03_strides = None, dense_01 = None, dense_02 = None):
+                 conv2d_03_strides = None, dense_01 = None, dense_02 = None, dense_03 = None):
         """
         Configuration class for model arguments
         
@@ -22,7 +23,7 @@ class ModelConfigs(dict):
         batch_size = None
         conv2d_01_filters = None
         conv2d_01_kernel = None,
-        conv2d__01_strides = None
+        conv2d_01_strides = None
         conv2d_02_filters = None
         conv2d_02_kernel = None
         conv2d_02_strides = None
@@ -31,6 +32,7 @@ class ModelConfigs(dict):
         conv2d_03_strides = None
         dense_01 = None
         dense_02 = None
+        dense_03 = None
         """
         
         super(ModelConfigs, self).__init__()
@@ -42,7 +44,7 @@ class ModelConfigs(dict):
         self['batch_size'] = batch_size
         self['conv2d_01_filters'] = conv2d_01_filters
         self['conv2d_01_kernel'] = conv2d_01_kernel
-        self['conv2d_01_strides'] = conv2d__01_strides
+        self['conv2d_01_strides'] = conv2d_01_strides
         self['conv2d_02_filters'] = conv2d_02_filters
         self['conv2d_02_kernel'] = conv2d_02_kernel
         self['conv2d_02_strides'] = conv2d_02_strides
@@ -51,10 +53,16 @@ class ModelConfigs(dict):
         self['conv2d_03_strides'] = conv2d_03_strides
         self['dense_01'] = dense_01
         self['dense_02'] = dense_02
+        self['dense_03'] = dense_03
         self['image_size'] = image_size
         
-def make_sequential_model(config: ModelConfigs):        
-    model = Sequential([Conv2D(filters = config['conv2d_01_filters'], kernel_size = config['conv2d_01_kernel'],
+
+def make_sequential_model(config: ModelConfigs):
+    resize_rescale = Sequential([Resizing(config['image_size'][0], config['image_size'][1]), Rescaling(1/255)])
+    
+    model = Sequential([InputLayer(input_shape = (None, None, 3)),
+                        resize_rescale,
+                        Conv2D(filters = config['conv2d_01_filters'], kernel_size = config['conv2d_01_kernel'],
                                strides = config['conv2d_01_strides'], padding = 'valid', activation = 'relu'),
                         BatchNormalization(),
                         MaxPool2D(pool_size = 2, strides = 2),
@@ -71,6 +79,8 @@ def make_sequential_model(config: ModelConfigs):
                         BatchNormalization(),
                         Dense(units = config['dense_02'], activation = 'relu'),
                         BatchNormalization(),
-                        Dense(units = 1, activation = 'sigmoid')])
+                        Dense(units = config['dense_03'], activation = 'relu'),
+                        BatchNormalization(),
+                        Dense(units = 3, activation = 'softmax')])
     
     return model
